b57cb759ba25566d03bf74846552d8527581b8a7
hadarlevy <94686791+hadarlevy@users.noreply.github.com>
2022-05-11 01:53:07 +0300

    ver 0.0

--- main.py
+++ main.py
@@ -1,103 +1,110 @@
 # This is a sample Python script.
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import math
+
+
+def calc_polynomial_in_point(polynomial1, point):
+    value = 0
+    for i in range(len(polynomial1)):
+        if i == (len(polynomial1) - 1):
+            value += polynomial1[i]
+            break
+        value += (point ** (len(polynomial1) - i - 1)) * (polynomial1[i])
+    return value
+
+
+def Bisection_Method(polynomial3, start_point, end_point, epsilon):
+    iterations_counter = 0
+    error_range = -(math.log((epsilon / abs(start_point-end_point)), math.e)) / math.log(2, math.e)
+    while end_point - start_point > epsilon:
+        iterations_counter += 1
+        if iterations_counter - 1 > error_range:
+            print("This polynomial does not converge on Bisection Method  ")
+            return
+        mid = (start_point + end_point) / 2
+        end_val = calc_polynomial_in_point(polynomial3, end_point)
+        mid_val = calc_polynomial_in_point(polynomial3, mid)
+        if end_val * mid_val > 0:
+            end_point = mid
+        else:
+            start_point = mid
+    if round(calc_polynomial_in_point(polynomial, mid)) != 0:
+        return
+    print(f'After {iterations_counter} iterations, the root is:')
+    return mid
+
+
+def general_function(polynomial2, start_point, end_point, choice):
+    arr_roots = []
+    index = 0
+    x0, x1 = start_point, start_point
+    while x1 < end_point:
+        x1 += 0.1
+        x1 = round(x1, 1)
+        val_x0 = calc_polynomial_in_point(polynomial2, x0)
+        val_x1 = calc_polynomial_in_point(polynomial2, x1)
+        if val_x0 * val_x1 < 0:
+            root = Bisection_Method(polynomial2, x0, x1, e)
+            if root in arr_roots or root is None:
+                continue
+            else:
+                arr_roots.append(root)
+                print(arr_roots[index])
+                index += 1
+        x0 = x1
+    return arr_roots
+
+
+def create_derive(polynomial_to_derive):
+    for i in range(len(polynomial_to_derive)):
+        if i == len(polynomial_to_derive) - 1:
+            polynomial_to_derive[i] = 0
+        polynomial_to_derive[i] = (len(polynomial_to_derive) - i - 1) * polynomial_to_derive[i]
+    polynomial_to_derive.remove(polynomial_to_derive[len(polynomial_to_derive)-1])
+    return polynomial_to_derive
+
+# Press the green button in the gutter to run the script.
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-def calc_polynomial(polynomial1,point):
-        sum1 = 0
-        for i in range(size):
-            if i == (size - 1):
-                sum1 += polynomial1[i]
-                break
 
-            sum1 += (point ** (size - i - 1)) * (polynomial1[i])
-        return sum1
-def Bisection_Method(polynomial, start_point, end_point, e):
-    index = 0
-    while abs(start_point - end_point) > e:
-        index += 1
-        mid = (start_point + end_point) / 2
-        sum_first = calc_polynomial(polynomial, start_point)
-        sum_mid = calc_polynomial(polynomial, mid)
-        if sum_first * sum_mid < 0:
-            start_point = mid
-        else:
-            end_point = mid
-
-
-    print(f'There was a {index} iterations')
-    return mid
-
-def general_function(polynomial, start_point, end_point, flag):
-    g = 0.1
-    arr = []
-    index = 0
-    num0, num1 = start_point, start_point
-    while num1 < end_point:
-
-        num1 += 0.1
-        if num0 == 0 or num1 == 0:
-            if num0 == 0:
-                sum_x0 = calc_polynomial(polynomial, num0)
-                if sum_x0 == 0:
-                    if sum_x0 in arr:
-                        continue
-                    else:
-                        arr.append(num0)
-                        print("This case in itself no need to run it")
-                        print(num0)
-                        index += 1
-            else:
-                sum_x1 = calc_polynomial(polynomial, num1)
-                if sum_x1 == 0:
-                    if sum_x1 in arr:
-                        continue
-                    else:
-                        arr.append(num1)
-                        print("This case in itself no need to run it")
-                        print(num1)
-                        index += 1
-        else:
-            sum_x0 = calc_polynomial(polynomial, num0)
-            sum_x1 = calc_polynomial(polynomial, num1)
-            if sum_x0 * sum_x1 < 0:
-                slice1 = Bisection_Method(polynomial, num0, num1, e)
-                if slice1 in arr:
-                    continue
-                else:
-                    arr.append(slice1)
-                    print(arr[index])
-                    index += 1
-        num0 = num1
-
-    return arr
-
-
-
-
-
-
-
-
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
     size = int(input("Enter the biggest power of the polynomial: "))
-    e = 0.0001
+    e = 0.00000000001
     arr = []
     print("press the coefficient of: ")
     for i in range(size+1):
         num = int(input(f'X^{size-i}= '))
         arr.append(num)
-    size += 1
     polynomial = tuple(arr)
-    print("press the range")
+    polynomial_derivative = tuple(create_derive(arr))
+    print(polynomial_derivative)
+    """print("Press the range")
+    start = input("start point: ")
+    end = input("end point: ")
+    print("Press the method you want to display on your polynomial:"
+          "1-Bisection method"
+          "2-Raphson-Newton method"
+          "3- Secant method")
+    choice = int(input(""))"""
     general_function(polynomial, -3, 2, 1)
+    general_function(polynomial_derivative, -3, 2, 1)
+
+
+
+
+
+
+
+
+
+
+
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
